d80976840d39d1c784cd7433eafee537e2008159
Vika Putri Ariyanti <vikaputriariyanti@gmail.com>
2022-01-17 19:56:37 +0700

    cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import CourseList from "./container/CourseList";
 import Detail from "./container/Detail";
+import Cart from "./container/Cart";
 import VideoPlayer from "./container/VideoPlayer";
 import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import './index.css';
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" component={CourseList} exact />
           <Route path="/course-detail" component={Detail} exact />
           <Route path="/course-video-player" component={VideoPlayer} exact />
+          <Route path="/cart" component={Cart} exact />
         </Switch>
       </div>
     </Router>
